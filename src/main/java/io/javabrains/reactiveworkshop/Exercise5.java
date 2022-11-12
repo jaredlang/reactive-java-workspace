dec9e2f7449a1184649d88ec989af08dd65ce9f1
Jared Lang <jaredlang76@gmail.com>
2022-11-12 15:56:28 -0600

    Completed Exercise 5 (handle other events)

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -2,6 +2,11 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.SignalType;
+
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
@@ -9,13 +14,63 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(
+            user -> System.out.println(user),
+            err -> System.out.println(err.getMessage()),
+            () -> System.out.println("User Mono Complete")
+        ); 
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>()); 
 
-        System.out.println("Press a key to end");
+        System.out.println("Press ENTER to end");
         System.in.read();
+    }
+
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    @Override
+    protected void hookOnNext(T value) {
+        System.out.println("MY: " + value.toString() + " received");
+        //request(2); //backpressure. Signal how many more the subscriber can handle RATHER THAN get the event
+        // If there is no back pressure. The subscriber won't be fired. 
+        // The best practice is to call the method on the super class. 
+        super.hookOnNext(value);
+    }
+
+    @Override
+    protected void hookFinally(SignalType type) {
+        System.out.println("MY: Finally tide-up");
+        super.hookFinally(type);
+    }
+
+    @Override
+    protected void hookOnCancel() {
+        System.out.println("MY: Subscription cancelled");
+        super.hookOnCancel();
+    }
+
+    @Override
+    protected void hookOnComplete() {
+        System.out.println("MY: All completed");
+        super.hookOnComplete();
+    }
+
+    @Override
+    protected void hookOnError(Throwable throwable) {
+        System.out.println("MY: Something went error");
+        super.hookOnError(throwable);
+    }
+
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("MY: Subscription happened");
+        //request(3); //backpressure. Signal how many more the subscriber can handle RATHER THAN get the event
+        // If there is no back pressure. The subscriber won't be fired. 
+        // The best practice is to call the method on the super class. 
+        super.hookOnSubscribe(subscription);
     }
 
 }
