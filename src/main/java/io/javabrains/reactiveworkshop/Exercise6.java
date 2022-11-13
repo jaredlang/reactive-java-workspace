53c02c88706b8cde442dad4c16dafdb10fabc216
Jared Lang <jaredlang76@gmail.com>
2022-11-13 11:54:56 -0600

    More blocking operators

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -10,16 +10,25 @@ public class Exercise6 {
 
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
 
-        // Get the value from the Mono into a String variable but give up after 5 seconds
-        String value = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5)); 
-        System.out.println(value);
+        // // Get the value from the Mono into a String variable but give up after 5 seconds
+        // String value = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5)); 
+        // System.out.println(value);
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        var valueList = ReactiveSources.unresponsiveFlux()
-            .window(Duration.ofSeconds(5))
+        // // Attempt 1: With window, not a solution
+        // var valueList = ReactiveSources.stringNumbersFlux()
+        //     .window(Duration.ofSeconds(5))
+        //     // After window, collectList returns Mono<List<Flux<String>>>
+        //     // Without window, collectList returns Mono<List<String>>
+        //     .collectList()
+        //     .block();
+        // Attempt 2: with block duration. It IS a solution
+        var valueList = ReactiveSources.stringNumbersFlux()
+            // collectList blocks the whole flux
             .collectList()
-            .block();
+            .block(Duration.ofSeconds(5));
+
         valueList.forEach(v -> {
             System.out.println(v);
         });
--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,20 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().count().subscribe(System.out::println); 
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().log().collectList().subscribe(System.out::println);
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        // Option 1: not so good. Every time you use function, you block a thread.
+        ReactiveSources.intNumbersFlux().log().buffer(2).subscribe(pair -> {
+            System.out.println(pair.get(0) + pair.get(1)); 
+        }); 
+        // Option 2: better. Use reactive operators as much as possible. 
+        ReactiveSources.intNumbersFlux().log().buffer(2)
+            .map(pair -> pair.get(0) + pair.get(1))
+            .subscribe(System.out::println); 
 
         System.out.println("Press a key to end");
         System.in.read();
