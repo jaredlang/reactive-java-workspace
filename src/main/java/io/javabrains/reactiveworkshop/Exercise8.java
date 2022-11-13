cb9d868fb1ac8431c0604338b4243cfd0b1b7876
Jared Lang <jaredlang76@gmail.com>
2022-11-13 11:36:27 -0600

    completed exercise 8 (exception handling)

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -12,14 +12,32 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                       .subscribe(
+                            i -> System.out.println(i), 
+                            // OnError terminates the subscription
+                            err -> System.out.println(err.getMessage())
+                        ); 
+
+        ReactiveSources.intNumbersFluxWithException()
+                       .doOnError(err -> System.out.println(err.getMessage()))
+                       // doOnError passes on the error and terminates the subscription
+                       .subscribe(i -> System.out.println(i)); 
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
-
+        ReactiveSources.intNumbersFluxWithException()
+                       .onErrorContinue((err, i) -> System.out.println(err.getMessage()))
+                       // onErrorContinue passes on the error but won't terminate the subscription
+                       .subscribe(i -> System.out.println(i)); 
+        
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                       //.onErrorReturn(-1)
+                       // onErrorReturn passes on the error but does terminate the subscription
+                       .onErrorResume(err -> Flux.just(-1, -2))
+                       // onErrorResume passes on the error but does terminate the subscription
+                       .subscribe(i -> System.out.println(i)); 
 
         System.out.println("Press a key to end");
         System.in.read();
